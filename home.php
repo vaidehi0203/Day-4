1899297fe9bfd4772f05548f76c4b18754fd9c8a
vaidehi0203 <84798150+vaidehi0203@users.noreply.github.com>
2021-06-02 16:44:05 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Anjali</title>
+
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+
+    <body>
+<?php
+include './partial/Menu.php';
+include './partial/logo.php';
+        
+?>
+<div id="wrapper"> 
+	<!-- end #header -->
+	<div id="page-bgtop"></div>
+	<div id="page">
+            <div><img src="a.jpg" width="920" height="300" alt="" /></div>
+		<div id="content">
+			<div class="post">
+				<h2 class="title"><a href="#">Welcome to my Proffesion </a></h2>
+				
+				<div style="clear: both;">&nbsp;</div>
+				<div class="entry">
+					Hi! Myself Anjali.
+				</div>
+			</div>
+			
+			</div>
+			<div style="clear: both;">&nbsp;</div>
+		</div>
+		<!-- end #content -->
+		<?php
+                include './partial/sidebar.php';
+                ?>
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<div id="page-bgbtm"></div>
+	<!-- end #page --> 
+<?php
+                include './partial/footer.php';
+?>
+<!-- end #footer -->
+</body>
+</html>
